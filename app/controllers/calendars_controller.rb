0051cd0514754de63ed898e77051b65fec5e6f63
prospike <best_diagram22@hotmail.co.jp>
2023-09-11 15:27:52 +0900

    Issue 4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,9 +15,11 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
+
+  
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
@@ -37,6 +39,6 @@ class CalendarsController < ApplicationController
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
-
   end
 end
+
